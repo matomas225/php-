609bc92f42ab5a989f74134eb3beeeb48b78f50b
Matomas <58953916+matomas225@users.noreply.github.com>
2020-10-13 11:16:37 +0300

    Update main.php
    diffrent structure

--- main.php
+++ main.php
@@ -5,152 +5,84 @@ $data = [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper ("Matas"),
             "Pavarde" => strtoupper ("Macijauskas"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
+            "DarbuVidurkis" => [
+              "MT" => rand(1,10),
+              "LT" => rand(1,10),
+              "AK" => rand(1,10)
             ],
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data2 = [
-    "KlaseB" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys2" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Jonas"),
             "Pavarde" => strtoupper("Petras"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data3 = [
-    "KlaseC" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys3" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Deimis"),
             "Pavarde" => strtoupper("Pussyslayer69"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data4 = [
-    "KlaseD" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys4" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Laimis"),
             "Pavarde" => strtoupper("LIL Lemon"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data5 = [
-    "KlaseE" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys5" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Jonas"),
             "Pavarde" => strtoupper("TheGamer"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data6 = [
-    "KlaseF" => [
-        "Mokinys1" => [
+        ],
+    ],
+    "KlaseB" => [
+        "Mokinys6" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Osva"),
             "Pavarde" => strtoupper("Bulka"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data7 = [
-    "KlaseG" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys7" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Gabija"),
             "Pavarde" => "BUKAuskaite",
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data8 = [
-    "KlaseH" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys8" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Lukas"),
             "Pavarde" => strtoupper("RapperMan"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data9 = [
-    "KlaseL" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys9" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Titas"),
             "Pavarde" => strtoupper("Chill"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
-        ]
-    ]
-];
-$data10 = [
-    "KlaseM" => [
-        "Mokinys1" => [
+        ],
+        "Mokinys10" => [
             "Kodas" => rand(0, 10000),
             "Vardas" => strtoupper("Vidmantas"),
             "Pavarde" => strtoupper("Animautas"),
-            "DarbuVidurkis" =>[
-                "MT" => 5,
-                "IT" => 9,
-                "AK" => 10
-            ],
+            "DarbuVidurkis" => 5,
             "DuomenuFormavimoData" => date("Y/m/d - H:i:s")
         ]
     ]
 ];
 ?>
+
 <style>
+
 table.customTable {
   width: 100%;
   background-color: #FF3030;
@@ -185,106 +117,17 @@ table.customTable thead {
     </tr>
   </thead>
   <tbody>
-    <tr>
       <?php foreach($data as $key => $value):?>
-      <td><?=$key;?></td>
+      <?php foreach($value as $value2 => $value3):?>
+        <tr>
+      <td><?=$key?></td>
+      <?php foreach($value3 as $value4 => $value5):?>
+      <td><?=$value5?></td>
       <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
+      </tr>
       <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data2 as $key => $value):?>
-      <td><?=$key;?></td>
       <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data3 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data4 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data5 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data6 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data7 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data8 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data9 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
-    <tr>
-    <?php foreach($data10 as $key => $value):?>
-      <td><?=$key;?></td>
-      <?php endforeach;?>
-      <?php foreach($value as $value2 =>$value3):?>
-        <?php endforeach;?>
-        <?php foreach($value3 as $value4 =>$value5):?>
-      <td><?=$value5;?></td>
-      <?php endforeach;?>
-    </tr>
   </tbody>
 </table>
+
 
